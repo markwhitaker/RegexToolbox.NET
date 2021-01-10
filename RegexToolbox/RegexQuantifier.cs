bb39dcdfe97f55c53ca83debc2e73d3669639e30
Mark Whitaker <git@mainwave.co.uk>
2021-01-10 08:46:48 +0000

    Removed unused name parameter from RegexQuantifier constructor (#35)

--- RegexToolbox/RegexQuantifier.cs
+++ RegexToolbox/RegexQuantifier.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace RegexToolbox
+﻿namespace RegexToolbox
 {
     /// <summary>
     /// Quantifiers that can be applied to regex elements or groups
@@ -10,11 +8,9 @@ namespace RegexToolbox
         #region Private property and constructor
 
         private string RegexString { get; set; }
-        internal string Name { get; private set; }
 
-        private RegexQuantifier(string name, string regexString)
+        private RegexQuantifier(string regexString)
         {
-            Name = name;
             RegexString = regexString;
         }
 
@@ -25,17 +21,17 @@ namespace RegexToolbox
         /// <summary>
         /// Quantifier to match the preceding element zero or more times
         /// </summary>
-        public static RegexGreedyQuantifier ZeroOrMore => new RegexGreedyQuantifier("ZeroOrMore", "*");
+        public static RegexGreedyQuantifier ZeroOrMore => new RegexGreedyQuantifier("*");
 
         /// <summary>
         /// Quantifier to match the preceding element one or more times
         /// </summary>
-        public static RegexGreedyQuantifier OneOrMore => new RegexGreedyQuantifier("OneOrMore", "+");
+        public static RegexGreedyQuantifier OneOrMore => new RegexGreedyQuantifier("+");
 
         /// <summary>
         /// Quantifier to match the preceding element once or not at all
         /// </summary>
-        public static RegexGreedyQuantifier ZeroOrOne => new RegexGreedyQuantifier("ZeroOrOne", "?");
+        public static RegexGreedyQuantifier ZeroOrOne => new RegexGreedyQuantifier("?");
 
         #endregion
 
@@ -45,22 +41,21 @@ namespace RegexToolbox
         /// Quantifier to match an exact number of occurrences of the preceding element
         /// </summary>
         /// <param name="times">The exact number of occurrences to match</param>
-        public static RegexQuantifier Exactly(int times) =>
-            new RegexQuantifier($"Exactly({times})", "{" + times + "}");
+        public static RegexQuantifier Exactly(int times) => new RegexQuantifier("{" + times + "}");
 
         /// <summary>
         /// Quantifier to match at least a minimum number of occurrences of the preceding element
         /// </summary>
         /// <param name="minimum">The minimum number of occurrences to match</param>
         public static RegexGreedyQuantifier AtLeast(int minimum) =>
-            new RegexGreedyQuantifier($"AtLeast({minimum})", "{" + minimum + ",}");
+            new RegexGreedyQuantifier("{" + minimum + ",}");
 
         /// <summary>
         /// Quantifier to match no more than a maximum number of occurrences of the preceding element
         /// </summary>
         /// <param name="maximum">The maximum number of occurrences to match</param>
         public static RegexGreedyQuantifier NoMoreThan(int maximum) =>
-            new RegexGreedyQuantifier($"NoMoreThan({maximum})", "{0," + maximum + "}");
+            new RegexGreedyQuantifier("{0," + maximum + "}");
 
         /// <summary>
         /// Quantifier to match at least a minimum, and no more than a maximum, occurrences of the preceding element
@@ -68,7 +63,7 @@ namespace RegexToolbox
         /// <param name="minimum">The minimum number of occurrences to match</param>
         /// <param name="maximum">The maximum number of occurrences to match</param>
         public static RegexGreedyQuantifier Between(int minimum, int maximum) =>
-            new RegexGreedyQuantifier($"Between({minimum}, {maximum})", "{" + minimum + "," + maximum + "}");
+            new RegexGreedyQuantifier("{" + minimum + "," + maximum + "}");
 
         public override string ToString()
         {
@@ -85,7 +80,7 @@ namespace RegexToolbox
         /// </summary>
         public sealed class RegexGreedyQuantifier : RegexQuantifier
         {
-            public RegexGreedyQuantifier(string name, string regexString) : base(name, regexString)
+            public RegexGreedyQuantifier(string regexString) : base(regexString)
             {
             }
 
@@ -97,7 +92,6 @@ namespace RegexToolbox
             {
                 get
                 {
-                    Name += ".ButAsFewAsPossible";
                     RegexString += "?";
                     return this;
                 }

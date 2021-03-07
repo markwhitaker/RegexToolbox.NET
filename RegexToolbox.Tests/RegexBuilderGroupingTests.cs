978b9548b39466bd5b6c98ec86ea0be9d1873fd4
Mark Whitaker <git@mainwave.co.uk>
2021-03-07 10:15:13 +0000

    Improve unit tests and file structure (#39)
    * Improve unit tests
    
    * Split RegexBuilder into partial class files
    
    * Split RegexBuilderTests into separate fixtures

---
+++ RegexToolbox.Tests/RegexBuilderGroupingTests.cs
@@ -0,0 +1,126 @@
+﻿using NUnit.Framework;
+using static RegexToolbox.RegexQuantifier;
+
+namespace RegexToolbox.Tests
+{
+    [TestFixture]
+    public class RegexBuilderGroupingTests
+    {
+        [Test]
+        public void TestGroup()
+        {
+            var regex = new RegexBuilder()
+                .Group(r => r
+                    .Text("a")
+                    .Digit())
+                .BuildRegex();
+
+            Assert.That(regex.ToString(), Is.EqualTo(@"(a\d)"));
+        }
+
+        [Test]
+        public void TestGroupWithQuantifier()
+        {
+            var regex = new RegexBuilder()
+                .Group(r => r
+                    .Text("a")
+                    .Digit(),
+                    OneOrMore)
+                .BuildRegex();
+
+            Assert.That(regex.ToString(), Is.EqualTo(@"(a\d)+"));
+        }
+
+        [Test]
+        public void TestNestedGroup()
+        {
+            var regex = new RegexBuilder()
+                .Group(r1 => r1
+                    .Text("a")
+                    .Group(r2 => r2
+                        .Digit())
+                    )
+                .BuildRegex();
+
+            Assert.That(regex.ToString(), Is.EqualTo(@"(a(\d))"));
+        }
+
+        [Test]
+        public void TestNonCapturingGroup()
+        {
+            var regex = new RegexBuilder()
+                .NonCapturingGroup(r => r
+                    .Text("a")
+                    .Digit())
+                .BuildRegex();
+
+            Assert.That(regex.ToString(), Is.EqualTo(@"(?:a\d)"));
+        }
+
+        [Test]
+        public void TestNonCapturingGroupWithQuantifier()
+        {
+            var regex = new RegexBuilder()
+                .NonCapturingGroup(r => r
+                    .Text("a")
+                    .Digit(),
+                    OneOrMore)
+                .BuildRegex();
+
+            Assert.That(regex.ToString(), Is.EqualTo(@"(?:a\d)+"));
+        }
+
+        [Test]
+        public void TestNestedNonCapturingGroup()
+        {
+            var regex = new RegexBuilder()
+                .NonCapturingGroup(r1 => r1
+                    .Text("a")
+                    .NonCapturingGroup(r2 => r2
+                        .Digit())
+                    )
+                .BuildRegex();
+
+            Assert.That(regex.ToString(), Is.EqualTo(@"(?:a(?:\d))"));
+        }
+
+        [Test]
+        public void TestNamedGroup()
+        {
+            var regex = new RegexBuilder()
+                .NamedGroup("group", r => r
+                    .Text("a")
+                    .Digit())
+                .BuildRegex();
+
+            Assert.That(regex.ToString(), Is.EqualTo(@"(?<group>a\d)"));
+        }
+
+        [Test]
+        public void TestNamedGroupWithQuantifier()
+        {
+            var regex = new RegexBuilder()
+                .NamedGroup("group", r => r
+                    .Text("a")
+                    .Digit(),
+                    OneOrMore)
+                .BuildRegex();
+
+            Assert.That(regex.ToString(), Is.EqualTo(@"(?<group>a\d)+"));
+        }
+
+        [Test]
+        public void TestNestedNamedGroup()
+        {
+            var regex = new RegexBuilder()
+                .NamedGroup("group1", r1 => r1
+                    .Text("a")
+                    .NamedGroup("group2", r2 => r2
+                        .Digit())
+                    )
+                .BuildRegex();
+
+            Assert.That(regex.ToString(), Is.EqualTo(@"(?<group1>a(?<group2>\d))"));
+        }
+    }
+}
---
+++ RegexToolbox/RegexBuilder.Grouping.cs
@@ -0,0 +1,99 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexToolbox
+{
+    public sealed partial class RegexBuilder
+    {
+        /// <summary>
+        /// Add a capture group to the regex. Capture groups have two purposes: they group part of the expression so it
+        /// can have quantifiers applied to it, and they capture the results of each group match and allow you to access
+        /// them afterwards using <see cref="Match.Groups"/>.
+        ///
+        /// If you don't want to capture the group match, use <see cref="NonCapturingGroup"/>.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// Regex regex = new RegexBuilder()
+        ///     .Group(r => r
+        ///         .Letter()
+        ///         .Digit())
+        ///     .BuildRegex();
+        /// </code>
+        /// </example>
+        /// <param name="groupElements">
+        /// A lambda containing the <see cref="RegexBuilder"/> elements required within the group
+        /// </param>
+        /// <param name="quantifier">Quantifier to apply to this group</param>
+        public RegexBuilder Group(
+            SubRegexBuilder groupElements,
+            RegexQuantifier quantifier = null)
+        {
+            AddPart("(");
+            groupElements(this);
+            return EndGroup(quantifier);
+        }
+
+        /// <summary>
+        /// Add a non-capturing group to the regex. Non-capturing groups group part of the expression so it can have
+        /// quantifiers applied to it, but do not capture the results of each group match, meaning you can't access them
+        /// afterwards using <see cref="Match.Groups"/>.
+        ///
+        /// If you want to capture group results, use <see cref="Group"/> or <see cref="NamedGroup"/>.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// Regex regex = new RegexBuilder()
+        ///     .NonCapturingGroup(r => r
+        ///         .Letter()
+        ///         .Digit())
+        ///     .BuildRegex();
+        /// </code>
+        /// </example>
+        /// <param name="groupElements">
+        /// A lambda containing the <see cref="RegexBuilder"/> elements required within the group
+        /// </param>
+        /// <param name="quantifier">Quantifier to apply to this group</param>
+        public RegexBuilder NonCapturingGroup(
+            SubRegexBuilder groupElements,
+            RegexQuantifier quantifier = null)
+        {
+            StartNonCapturingGroup();
+            groupElements(this);
+            return EndGroup(quantifier);
+        }
+
+        /// <summary>
+        /// Add a named capture group to the regex. Capture groups have two purposes: they group part of the expression
+        /// so it can have quantifiers applied to it, and they capture the results of each group match and allow you to
+        /// access them afterwards using <see cref="Match.Groups"/>. Named capture groups can be accessed by indexing
+        /// into <see cref="Match.Groups"/> using either the assigned name or a numerical index.
+        ///
+        /// If you don't want to name the group match, use <see cref="Group"/>.
+        ///
+        /// If you don't want to capture the group match, use <see cref="NonCapturingGroup"/>.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// Regex regex = new RegexBuilder()
+        ///     .NamedGroup("name", r => r
+        ///         .Letter()
+        ///         .Digit())
+        ///     .BuildRegex();
+        /// </code>
+        /// </example>
+        /// <param name="name">Name that can be used to access the group from <see cref="Match.Groups"/></param>
+        /// <param name="groupElements">
+        /// A lambda containing the <see cref="RegexBuilder"/> elements required within the group
+        /// </param>
+        /// <param name="quantifier">Quantifier to apply to this group</param>
+        public RegexBuilder NamedGroup(
+            string name,
+            SubRegexBuilder groupElements,
+            RegexQuantifier quantifier = null)
+        {
+            AddPart($"(?<{name}>");
+            groupElements(this);
+            return EndGroup(quantifier);
+        }
+    }
+}

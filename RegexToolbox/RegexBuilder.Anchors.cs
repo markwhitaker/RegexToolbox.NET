3d7e9ba441c293f378b194465d69e1c749c86d1c
Mark Whitaker <markw@bitrot.net>
2024-03-29 08:43:53 +0000

    Update language version

--- RegexToolbox/RegexBuilder.Anchors.cs
+++ RegexToolbox/RegexBuilder.Anchors.cs
@@ -1,21 +1,20 @@
-﻿namespace RegexToolbox
+﻿namespace RegexToolbox;
+
+public sealed partial class RegexBuilder
 {
-    public sealed partial class RegexBuilder
-    {
-        /// <summary>
-        /// Add a zero-width anchor element to match the start of the string
-        /// </summary>
-        public RegexBuilder StartOfString() => AddPart("^");
+    /// <summary>
+    /// Add a zero-width anchor element to match the start of the string
+    /// </summary>
+    public RegexBuilder StartOfString() => AddPart("^");
 
-        /// <summary>
-        /// Add a zero-width anchor element to match the end of the string
-        /// </summary>
-        public RegexBuilder EndOfString() => AddPart("$");
+    /// <summary>
+    /// Add a zero-width anchor element to match the end of the string
+    /// </summary>
+    public RegexBuilder EndOfString() => AddPart("$");
 
-        /// <summary>
-        /// Add a zero-width anchor element to match the boundary between an alphanumeric/underscore character
-        /// and either a non-alphanumeric, non-underscore character or the start/end of the string.
-        /// </summary>
-        public RegexBuilder WordBoundary() => AddPart(@"\b");
-    }
+    /// <summary>
+    /// Add a zero-width anchor element to match the boundary between an alphanumeric/underscore character
+    /// and either a non-alphanumeric, non-underscore character or the start/end of the string.
+    /// </summary>
+    public RegexBuilder WordBoundary() => AddPart(@"\b");
 }
